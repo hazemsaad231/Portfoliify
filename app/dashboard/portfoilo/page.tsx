432a4135a969c7f92c31072e4c34a98ee31e66fb
hazem saad <hazemsaad231@gmail.com>
2026-02-16 10:48:35 +0200

    up

--- app/dashboard/portfoilo/page.tsx
+++ app/dashboard/portfoilo/page.tsx
@@ -1,7 +1,7 @@
 
 
 "use client";
-import { toast } from "react-toastify";
+import { toast } from "react-hot-toast";
 import { useEffect, useState } from 'react';
 import { supabase } from '../../utils/supabase';
 import { ExternalLink, Copy, LayoutDashboard, User, RefreshCw } from 'lucide-react';
